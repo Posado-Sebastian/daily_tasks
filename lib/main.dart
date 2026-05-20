4039a9c2fa2f8facac9632a17fba80114c9ad62b
Posado-Sebastian <sebastianposado@gmail.com>
2026-05-20 19:59:19 +0000

    navbar and date added

--- lib/main.dart
+++ lib/main.dart
@@ -4,15 +4,33 @@ void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  int _selectedIndex = 0;
+
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        appBar: AppBar(
+          title: Text('${DateTime.now().month}/${DateTime.now().day}/${DateTime.now().year}'),
+          centerTitle: true,
+        ),
+        body: const Center(child: Text('Hello World!')),
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _selectedIndex,
+          onTap: (index) => setState(() => _selectedIndex = index),
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.settings), label: 'Config'),
+            BottomNavigationBarItem(icon: Icon(Icons.add_circle), label: 'Agregar'),
+            BottomNavigationBarItem(icon: Icon(Icons.calendar_today), label: 'Calendario'),
+          ],
         ),
       ),
     );
